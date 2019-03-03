c2c3cbd512d4af387626fe66e791cf6db34b92e5
mrpodato <41372461+mrpodato@users.noreply.github.com>
2019-03-03 19:49:04 +0800

    Add files via upload

---
+++ calculator addition.py
@@ -0,0 +1,8 @@
+#enter the 2 numbers you want added together
+print ("enter the first number")
+FirstNum = input()
+print ("enter the second number")
+SecondNum = input()
+sum = float(FirstNum) + float(SecondNum)
+print ("your answer is: ", (sum))
+input()
